9391cf7f2d3ab53415a95e518d5f66d11c05c697
Flavia Missi <flaviamissi@gmail.com>
2013-03-07 17:16:58 -0300

    setup: created simple install script for varnish on ubuntu

---
+++ setup/install.sh
@@ -0,0 +1,4 @@
+curl http://repo.varnish-cache.org/debian/GPG-key.txt | sudo apt-key add -
+echo "deb http://repo.varnish-cache.org/ubuntu/ precise varnish-3.0" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install varnish -y
