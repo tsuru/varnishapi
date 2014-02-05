8709bc14ae17966f8648349fc664a6d343e119aa
Francisco Souza <fss@corp.globo.com>
2014-02-05 15:06:55 -0200

    storage: add dumb implementation for storage

---
+++ varnishapi/storage.py
@@ -0,0 +1,14 @@
+# Copyright 2014 varnishapi authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+class DumbStorage(object):
+
+    def store(self, *args, **kwargs):
+        pass
+
+    def retrieve(self, *args, **kwargs):
+        pass
+
+    def delete(self, *args, **kwargs):
+        pass
