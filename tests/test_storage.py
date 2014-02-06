028fd56b9cd78554db1594341288a63d71a371c5
Francisco Souza <fss@corp.globo.com>
2014-02-06 16:24:40 -0200

    storage: implement MongoDBStorage

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -5,6 +5,8 @@
 import json
 import unittest
 
+import pymongo
+
 from varnishapi import storage
 
 
@@ -26,3 +28,47 @@ class InstanceTestCase(unittest.TestCase):
         expected = {"id": "i-0800", "dns_name": "instance.cloud.tsuru.io",
                     "name": "myinstance"}
         self.assertEqual(expected, json.loads(json_str))
+
+
+class MongoDBStorageTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.client = pymongo.MongoClient('localhost', 27017)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.client.drop_database("feaas_test")
+
+    def setUp(self):
+        self.storage = storage.MongoDBStorage(dbname="feaas_test")
+
+    def test_store(self):
+        instance = storage.Instance(id="i-0800", name="secret",
+                                    dns_name="secret.pos.com")
+        self.storage.store(instance)
+        instance = self.client.feaas_test.instances.find_one({"name": "secret"})
+        expected = {"id": "i-0800", "name": "secret",
+                    "dns_name": "secret.pos.com", "_id": instance["_id"]}
+        self.assertEqual(expected, instance)
+
+    def test_retrieve(self):
+        expected = storage.Instance(id="i-0800", name="what",
+                                    dns_name="secret.pos.com")
+        self.storage.store(expected)
+        got = self.storage.retrieve("what")
+        self.assertEqual(expected.to_dict(), got.to_dict())
+
+    def test_retrieve_not_found(self):
+        with self.assertRaises(ValueError) as cm:
+            self.storage.retrieve("secret")
+        exc = cm.exception
+        self.assertEqual(("Instance not found",),
+                         exc.args)
+
+    def test_remove(self):
+        instance = storage.Instance(id="i-0800", name="years",
+                                    dns_name="secret.pos.com")
+        self.storage.store(instance)
+        self.storage.remove(instance.name)
+        self.assertIsNone(self.client.feaas_test.instances.find_one({"name": instance.name}))
--- varnishapi/storage.py
+++ varnishapi/storage.py
@@ -4,6 +4,8 @@
 
 import json
 
+import pymongo
+
 
 class Instance(object):
 
@@ -18,3 +20,26 @@ class Instance(object):
     def to_dict(self):
         return {"name": self.name, "dns_name": self.dns_name,
                 "id": self.id}
+
+
+class MongoDBStorage(object):
+
+    def __init__(self, mongo_uri=None, dbname=None):
+        mongo_uri = mongo_uri or "mongodb://localhost:27017/"
+        dbname = dbname or "feaas"
+        client = pymongo.MongoClient(mongo_uri)
+        self.db = client[dbname]
+        self.collection_name = "instances"
+
+    def store(self, instance):
+        self.db[self.collection_name].insert(instance.to_dict())
+
+    def retrieve(self, name):
+        instance = self.db[self.collection_name].find_one({"name": name})
+        if not instance:
+            raise ValueError("Instance not found")
+        del instance["_id"]
+        return Instance(**instance)
+
+    def remove(self, name):
+        self.db[self.collection_name].remove({"name": name})
