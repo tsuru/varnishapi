8db3d9257936cc4d558f09216864e41adc0b887f
Francisco Souza <fss@corp.globo.com>
2014-02-12 11:29:25 -0200

    setup: do not declare support to untested Python versions

--- setup.py
+++ setup.py
@@ -17,9 +17,8 @@ setup(
     author="CobraTeam",
     author_email="tsuru@corp.globo.com",
     classifiers=[
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-    ] + [("Programming Language :: Python :: %s" % x) for x in "2.6 2.7 3.0 3.1 3.2 3.3".split()],
+        "Programming Language :: Python :: 2.7",
+    ]
     packages=find_packages(exclude=["docs", "tests", "samples"]),
     include_package_data=True,
     install_requires=["Flask==0.9", "boto==2.25.0", "pymongo==2.6.3"],
