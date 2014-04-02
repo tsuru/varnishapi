e0e23d3e7c471a341a78d1db6979c74f1440124f
Francisco Souza <fss@corp.globo.com>
2014-04-02 10:59:36 -0300

    setup: list python-varnish

--- setup.py
+++ setup.py
@@ -23,5 +23,6 @@ setup(
     ],
     packages=find_packages(exclude=["docs", "tests", "samples"]),
     include_package_data=True,
-    install_requires=["Flask==0.9", "boto==2.25.0", "pymongo==2.6.3"],
+    install_requires=["Flask==0.9", "boto==2.25.0", "pymongo==2.6.3",
+                      "python-varnish==0.2.1"],
 )
