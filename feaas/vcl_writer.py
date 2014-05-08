4dcc0a7f6b86aa3a768113f02dda7797163b22b2
Francisco Souza <fss@corp.globo.com>
2014-05-08 18:20:44 -0300

    vcl_writer: implement loop, stop and all the things

--- feaas/vcl_writer.py
+++ feaas/vcl_writer.py
@@ -2,12 +2,43 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+import telnetlib
+import time
+
 
 class VCLWriter(object):
 
-    def __init__(self, interval=10, max_items=None):
+    def __init__(self, storage, interval=10, max_items=None):
+        self.storage = storage
         self.interval = interval
         self.max_items = max_items
 
     def loop(self):
-        pass
+        self.running = True
+        while self.running:
+            self.run()
+            time.sleep(self.interval)
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+        self.storage.lock_vcl_writer()
+        try:
+            units = self.storage.load_units("creating", limit=self.max_items)
+            up_units = []
+            for unit in units:
+                if self._is_unit_up(unit):
+                    up_units.append(unit)
+            if up_units:
+                self.storage.update_units(up_units, state="started")
+        finally:
+            self.storage.unlock_vcl_writer()
+
+    def _is_unit_up(self, unit):
+        try:
+            client = telnetlib.Telnet(unit.dns_name, "6082", timeout=3)
+            client.close()
+            return True
+        except:
+            return False
--- tests/test_vcl_writer.py
+++ tests/test_vcl_writer.py
@@ -2,14 +2,68 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+import threading
+import time
 import unittest
 
-from feaas import vcl_writer
+import mock
+
+from feaas import storage, vcl_writer
 
 
 class VCLWriterTestCase(unittest.TestCase):
 
     def test_init(self):
-        writer = vcl_writer.VCLWriter(interval=10, max_items=3)
+        strg = storage.MongoDBStorage()
+        writer = vcl_writer.VCLWriter(strg, interval=10, max_items=3)
+        self.assertEqual(strg, writer.storage)
         self.assertEqual(10, writer.interval)
         self.assertEqual(3, writer.max_items)
+
+    def test_loop(self):
+        fake_run = mock.Mock()
+        writer = vcl_writer.VCLWriter(None, interval=3, max_items=3)
+        writer.run = fake_run
+        t = threading.Thread(target=writer.loop)
+        t.start()
+        time.sleep(1)
+        writer.stop()
+        t.join()
+        fake_run.assert_called()
+
+    def test_stop(self):
+        writer = vcl_writer.VCLWriter(None)
+        writer.running = True
+        writer.stop()
+        self.assertFalse(writer.running)
+
+    def test_run(self):
+        units = [storage.Unit(dns_name="instance1.cloud.tsuru.io", id="i-0800"),
+                 storage.Unit(dns_name="instance2.cloud.tsuru.io", id="i-0801"),
+                 storage.Unit(dns_name="instance3.cloud.tsuru.io", id="i-0802")]
+        strg = mock.Mock()
+        strg.load_units.return_value = units
+        writer = vcl_writer.VCLWriter(strg, max_items=3)
+        writer._is_unit_up = lambda unit: unit == units[1]
+        writer.run()
+        strg.lock_vcl_writer.assert_called_once()
+        strg.load_units.assert_called_with("creating", limit=3)
+        strg.unlock_vcl_writer.assert_called_once()
+
+    @mock.patch("telnetlib.Telnet")
+    def test_is_unit_up_up(self, Telnet):
+        telnet_client = mock.Mock()
+        Telnet.return_value = telnet_client
+        unit = storage.Unit(dns_name="instance1.cloud.tsuru.io")
+        writer = vcl_writer.VCLWriter(None, max_items=3)
+        self.assertTrue(writer._is_unit_up(unit))
+        Telnet.assert_called_with(unit.dns_name, "6082", timeout=3)
+        telnet_client.close.assert_called_once()
+
+    @mock.patch("telnetlib.Telnet")
+    def test_is_unit_up_down(self, Telnet):
+        Telnet.side_effect = ValueError()
+        unit = storage.Unit(dns_name="instance1.cloud.tsuru.io")
+        writer = vcl_writer.VCLWriter(None, max_items=3)
+        self.assertFalse(writer._is_unit_up(unit))
+        Telnet.assert_called_with(unit.dns_name, "6082", timeout=3)
