2297d3bdaec325c1b29187408a1c851bcbf1b771
Francisco Souza <fss@corp.globo.com>
2014-05-12 10:37:49 -0300

    run: fix import

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Copyright 2014 varnishapi authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
-from varnishapi import api
+
+from feaas import api
 
 api.api.run(debug=True)
