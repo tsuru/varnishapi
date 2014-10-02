814fcd7c77c8af28e5a09af4e40cbb1a867244e1
Francisco Souza <fss@corp.globo.com>
2014-10-02 18:26:24 -0300

    managers/cloudstack: implement required methods for CloudStack
    This mean we now have CloudStack support, coming now CloudStack +
    GloboNetworkAPI :)

--- feaas/managers/cloudstack.py
+++ feaas/managers/cloudstack.py
@@ -3,8 +3,11 @@
 # license that can be found in the LICENSE file.
 
 import os
+import sys
+import time
+import uuid
 
-from feaas import managers
+from feaas import managers, storage
 
 from .cloudstack_client import CloudStack
 
@@ -25,14 +28,98 @@ class CloudStackManager(managers.BaseManager):
             raise MissConfigurationError("env var {} is required".format(name))
 
     def start_instance(self, name):
-        raise NotImplementedError()
+        instance = self.storage.retrieve_instance(name=name)
+        self._add_units(instance, 1)
+        return instance
 
     def terminate_instance(self, name):
-        raise NotImplementedError()
+        instance = self.storage.retrieve_instance(name=name)
+        for unit in instance.units:
+            self._destroy_vm(unit)
+        return instance
 
     def physical_scale(self, instance, quantity):
-        raise NotImplementedError()
+        new_units = quantity - len(instance.units)
+        if new_units < 0:
+            return self._remove_units(instance, -1 * new_units)
+        return self._add_units(instance, new_units)
+
+    def _add_units(self, instance, quantity):
+        units = []
+        for i in xrange(quantity):
+            unit = self._deploy_vm(instance)
+            instance.add_unit(unit)
+            units.append(unit)
+        self.storage.store_instance(instance)
+        return units
+
+    def _deploy_vm(self, instance):
+        secret = unicode(uuid.uuid4())
+        group = os.environ.get("CLOUDSTACK_GROUP", "feaas")
+        data = {
+            "group": group,
+            "displayname": "{}_{}".format(group, instance.name),
+            "templateid": self.get_env("CLOUDSTACK_TEMPLATE_ID"),
+            "zoneid": self.get_env("CLOUDSTACK_ZONE_ID"),
+            "serviceofferingid": self.get_env("CLOUDSTACK_SERVICE_OFFERING_ID"),
+            "userdata": self.client.encode_user_data(self.get_user_data(secret)),
+        }
+        project_id = os.environ.get("CLOUDSTACK_PROJECT_ID")
+        if project_id:
+            data["projectid"] = project_id
+        network_id = os.environ.get("CLOUDSTACK_NETWORK_ID")
+        if network_id:
+            data["networkid"] = network_id
+        vm_job = self.client.deployVirtualMachine(data)
+        max_tries = int(os.environ.get("CLOUDSTACK_MAX_TRIES", 100))
+        vm = self._wait_for_unit(vm_job, max_tries, project_id)
+        return storage.Unit(id=vm["id"], dns_name=vm["nic"][0]["ipaddress"],
+                            state="creating", secret=secret)
+
+    def _wait_for_unit(self, vm_job, max_tries, project_id):
+        status = 0
+        tries = 0
+        job_id = vm_job["jobid"]
+        while tries < max_tries:
+            result = self.client.queryAsyncJobResult({"jobid": job_id})
+            status = result["jobstatus"]
+            if status != 0:
+                break
+            time.sleep(1)
+            tries += 1
+        if status == 0:
+            raise MaxTryExceededError(max_tries)
+        data = {"id": vm_job["id"]}
+        if project_id:
+            data["projectid"] = project_id
+        vms = self.client.listVirtualMachines(data)
+        return vms["virtualmachine"][0]
+
+    def _remove_units(self, instance, quantity):
+        units = []
+        for i in xrange(quantity):
+            self._destroy_vm(instance.units[i])
+            units.append(instance.units[i])
+        for unit in units:
+            instance.remove_unit(unit)
+        self.storage.store_instance(instance)
+        return units
+
+    def _destroy_vm(self, unit):
+        try:
+            self.client.destroyVirtualMachine({"id": unit.id})
+        except Exception as e:
+            sys.stderr.write("[ERROR] Failed to terminate CloudStack VM: %s" %
+                             " ".join([str(arg) for arg in e.args]))
 
 
 class MissConfigurationError(Exception):
     pass
+
+
+class MaxTryExceededError(Exception):
+
+    def __init__(self, max_tries):
+        self.max_tries = max_tries
+        msg = "exceeded {0} tries".format(max_tries)
+        super(MaxTryExceededError, self).__init__(msg)
--- tests/test_cloudstack_manager.py
+++ tests/test_cloudstack_manager.py
@@ -2,27 +2,53 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+import copy
 import os
 import unittest
 
+import mock
+
+from feaas import storage
 from feaas.managers import cloudstack
 
 
 class CloudStackManagerTestCase(unittest.TestCase):
 
-    def set_envs(self, url="http://cloudstackapi", api_key="key",
-                 secret_key="secret"):
+    def set_api_envs(self, url="http://cloudstackapi", api_key="key",
+                     secret_key="secret"):
         os.environ["CLOUDSTACK_API_URL"] = self.url = url
         os.environ["CLOUDSTACK_API_KEY"] = self.api_key = api_key
         os.environ["CLOUDSTACK_SECRET_KEY"] = self.secret_key = secret_key
 
-    def del_envs(self):
-        del os.environ["CLOUDSTACK_API_URL"], os.environ["CLOUDSTACK_API_KEY"], \
-            os.environ["CLOUDSTACK_SECRET_KEY"]
+    def del_api_envs(self):
+        self._remove_envs("CLOUDSTACK_API_URL", "CLOUDSTACK_API_KEY",
+                          "CLOUDSTACK_SECRET_KEY")
+
+    def set_vm_envs(self, template_id="abc123", zone_id="zone1",
+                    service_offering_id="qwe123", project_id=None,
+                    network_id=None):
+        os.environ["CLOUDSTACK_TEMPLATE_ID"] = self.template_id = template_id
+        self.service_offering_id = service_offering_id
+        os.environ["CLOUDSTACK_SERVICE_OFFERING_ID"] = self.service_offering_id
+        os.environ["CLOUDSTACK_ZONE_ID"] = self.zone_id = zone_id
+        if project_id:
+            os.environ["CLOUDSTACK_PROJECT_ID"] = self.project_id = project_id
+        if network_id:
+            os.environ["CLOUDSTACK_NETWORK_ID"] = self.network_id = network_id
+
+    def del_vm_envs(self):
+        self._remove_envs("CLOUDSTACK_TEMPLATE_ID", "CLOUDSTACK_SERVICE_OFFERING_ID",
+                          "CLOUDSTACK_ZONE_ID", "CLOUDSTACK_PROJECT_ID",
+                          "CLOUDSTACK_NETWORK_ID")
+
+    def _remove_envs(self, *envs):
+        for env in envs:
+            if env in os.environ:
+                del os.environ[env]
 
     def test_init(self):
-        self.set_envs()
-        self.addCleanup(self.del_envs)
+        self.set_api_envs()
+        self.addCleanup(self.del_api_envs)
         client = cloudstack.CloudStackManager(storage=None)
         self.assertEqual(self.url, client.client.api_url)
         self.assertEqual(self.api_key, client.client.api_key)
@@ -39,8 +65,7 @@ class CloudStackManagerTestCase(unittest.TestCase):
         os.environ["CLOUDSTACK_API_URL"] = "something"
         with self.assertRaises(cloudstack.MissConfigurationError) as cm:
             cloudstack.CloudStackManager(storage=None)
-        self.set_envs()
-        self.addCleanup(self.del_envs)
+        self.addCleanup(self.del_api_envs)
         exc = cm.exception
         self.assertEqual(("env var CLOUDSTACK_API_KEY is required",),
                          exc.args)
@@ -50,8 +75,235 @@ class CloudStackManagerTestCase(unittest.TestCase):
         os.environ["CLOUDSTACK_API_KEY"] = "not_secret"
         with self.assertRaises(cloudstack.MissConfigurationError) as cm:
             cloudstack.CloudStackManager(storage=None)
-        self.set_envs()
-        self.addCleanup(self.del_envs)
+        self.addCleanup(self.del_api_envs)
         exc = cm.exception
         self.assertEqual(("env var CLOUDSTACK_SECRET_KEY is required",),
                          exc.args)
+
+    @mock.patch("uuid.uuid4")
+    def test_start_instance(self, uuid):
+        self.set_api_envs()
+        self.addCleanup(self.del_api_envs)
+        self.set_vm_envs(project_id="project-123", network_id="net-123")
+        self.addCleanup(self.del_vm_envs)
+        uuid.return_value = "uuid_val"
+        instance = storage.Instance(name="some_instance", units=[])
+        strg_mock = mock.Mock()
+        strg_mock.retrieve_instance.return_value = instance
+        client_mock = mock.Mock()
+        client_mock.deployVirtualMachine.return_value = {"id": "abc123",
+                                                         "jobid": "qwe321"}
+        client_mock.queryAsyncJobResult.return_value = {"jobstatus": 1}
+        vm = {"id": "abc123", "nic": [{"ipaddress": "10.0.0.1"}]}
+        client_mock.listVirtualMachines.return_value = {"virtualmachine": [vm]}
+        client_mock.encode_user_data.return_value = user_data = mock.Mock()
+        manager = cloudstack.CloudStackManager(storage=strg_mock)
+        manager.client = client_mock
+        got_instance = manager.start_instance("some_instance")
+        self.assertEqual(instance, got_instance)
+        self.assertEqual(1, len(instance.units))
+        unit = instance.units[0]
+        self.assertEqual("abc123", unit.id)
+        self.assertEqual("uuid_val", unit.secret)
+        self.assertEqual(instance, unit.instance)
+        self.assertEqual("10.0.0.1", unit.dns_name)
+        self.assertEqual("creating", unit.state)
+        strg_mock.retrieve_instance.assert_called_with(name="some_instance")
+        create_data = {"group": "feaas", "displayname": "feaas_some_instance",
+                       "templateid": self.template_id, "zoneid": self.zone_id,
+                       "serviceofferingid": self.service_offering_id,
+                       "userdata": user_data, "networkid": self.network_id,
+                       "projectid": self.project_id}
+        client_mock.deployVirtualMachine.assert_called_with(create_data)
+        actual_user_data = manager.get_user_data("uuid_val")
+        client_mock.encode_user_data.assert_called_with(actual_user_data)
+
+    @mock.patch("uuid.uuid4")
+    def test_start_instance_no_project_id(self, uuid):
+        self.set_api_envs()
+        self.addCleanup(self.del_api_envs)
+        self.set_vm_envs(network_id="net-123")
+        self.addCleanup(self.del_vm_envs)
+        uuid.return_value = "uuid_val"
+        instance = storage.Instance(name="some_instance", units=[])
+        strg_mock = mock.Mock()
+        strg_mock.retrieve_instance.return_value = instance
+        client_mock = mock.Mock()
+        client_mock.deployVirtualMachine.return_value = {"id": "abc123",
+                                                         "jobid": "qwe321"}
+        client_mock.queryAsyncJobResult.return_value = {"jobstatus": 1}
+        vm = {"id": "abc123", "nic": [{"ipaddress": "10.0.0.1"}]}
+        client_mock.listVirtualMachines.return_value = {"virtualmachine": [vm]}
+        client_mock.encode_user_data.return_value = user_data = mock.Mock()
+        manager = cloudstack.CloudStackManager(storage=strg_mock)
+        manager.client = client_mock
+        got_instance = manager.start_instance("some_instance")
+        self.assertEqual(instance, got_instance)
+        self.assertEqual(1, len(instance.units))
+        unit = instance.units[0]
+        self.assertEqual("abc123", unit.id)
+        self.assertEqual("uuid_val", unit.secret)
+        self.assertEqual(instance, unit.instance)
+        self.assertEqual("10.0.0.1", unit.dns_name)
+        self.assertEqual("creating", unit.state)
+        strg_mock.retrieve_instance.assert_called_with(name="some_instance")
+        create_data = {"group": "feaas", "displayname": "feaas_some_instance",
+                       "templateid": self.template_id, "zoneid": self.zone_id,
+                       "serviceofferingid": self.service_offering_id,
+                       "userdata": user_data, "networkid": self.network_id}
+        client_mock.deployVirtualMachine.assert_called_with(create_data)
+        actual_user_data = manager.get_user_data("uuid_val")
+        client_mock.encode_user_data.assert_called_with(actual_user_data)
+
+    @mock.patch("uuid.uuid4")
+    def test_start_instance_no_network_id(self, uuid):
+        self.set_api_envs()
+        self.addCleanup(self.del_api_envs)
+        self.set_vm_envs(project_id="proj-123")
+        self.addCleanup(self.del_vm_envs)
+        uuid.return_value = "uuid_val"
+        instance = storage.Instance(name="some_instance", units=[])
+        strg_mock = mock.Mock()
+        strg_mock.retrieve_instance.return_value = instance
+        client_mock = mock.Mock()
+        client_mock.deployVirtualMachine.return_value = {"id": "abc123",
+                                                         "jobid": "qwe321"}
+        client_mock.queryAsyncJobResult.return_value = {"jobstatus": 1}
+        vm = {"id": "abc123", "nic": [{"ipaddress": "10.0.0.1"}]}
+        client_mock.listVirtualMachines.return_value = {"virtualmachine": [vm]}
+        client_mock.encode_user_data.return_value = user_data = mock.Mock()
+        manager = cloudstack.CloudStackManager(storage=strg_mock)
+        manager.client = client_mock
+        got_instance = manager.start_instance("some_instance")
+        self.assertEqual(instance, got_instance)
+        self.assertEqual(1, len(instance.units))
+        unit = instance.units[0]
+        self.assertEqual("abc123", unit.id)
+        self.assertEqual("uuid_val", unit.secret)
+        self.assertEqual(instance, unit.instance)
+        self.assertEqual("10.0.0.1", unit.dns_name)
+        self.assertEqual("creating", unit.state)
+        strg_mock.retrieve_instance.assert_called_with(name="some_instance")
+        create_data = {"group": "feaas", "displayname": "feaas_some_instance",
+                       "templateid": self.template_id, "zoneid": self.zone_id,
+                       "serviceofferingid": self.service_offering_id,
+                       "userdata": user_data, "projectid": self.project_id}
+        client_mock.deployVirtualMachine.assert_called_with(create_data)
+        actual_user_data = manager.get_user_data("uuid_val")
+        client_mock.encode_user_data.assert_called_with(actual_user_data)
+
+    def test_start_instance_timeout(self):
+        def cleanup():
+            del os.environ["CLOUDSTACK_MAX_TRIES"]
+        self.addCleanup(cleanup)
+        os.environ["CLOUDSTACK_MAX_TRIES"] = "1"
+        self.set_api_envs()
+        self.addCleanup(self.del_api_envs)
+        self.set_vm_envs()
+        self.addCleanup(self.del_vm_envs)
+        instance = storage.Instance(name="some_instance", units=[])
+        strg_mock = mock.Mock()
+        strg_mock.retrieve_instance.return_value = instance
+        client_mock = mock.Mock()
+        client_mock.deployVirtualMachine.return_value = {"id": "abc123",
+                                                         "jobid": "qwe321"}
+        client_mock.queryAsyncJobResult.return_value = {"jobstatus": 0}
+        manager = cloudstack.CloudStackManager(storage=strg_mock)
+        manager.client = client_mock
+        with self.assertRaises(cloudstack.MaxTryExceededError) as cm:
+            manager.start_instance("some_instance")
+        exc = cm.exception
+        self.assertEqual(1, exc.max_tries)
+
+    def test_terminate_instance(self):
+        self.set_api_envs()
+        self.addCleanup(self.del_api_envs)
+        instance = storage.Instance(name="some_instance",
+                                    units=[storage.Unit(id="vm-123"),
+                                           storage.Unit(id="vm-456")])
+        strg_mock = mock.Mock()
+        strg_mock.retrieve_instance.return_value = instance
+        manager = cloudstack.CloudStackManager(storage=strg_mock)
+        manager.client = client_mock = mock.Mock()
+        got_instance = manager.terminate_instance("some_instance")
+        self.assertEqual(instance, got_instance)
+        expected_calls = [mock.call({"id": "vm-123"}), mock.call({"id": "vm-456"})]
+        self.assertEqual(expected_calls, client_mock.destroyVirtualMachine.call_args_list)
+
+    @mock.patch("sys.stderr")
+    def test_terminate_instance_ignores_exceptions(self, stderr):
+        self.set_api_envs()
+        self.addCleanup(self.del_api_envs)
+        instance = storage.Instance(name="some_instance",
+                                    units=[storage.Unit(id="vm-123"),
+                                           storage.Unit(id="vm-456")])
+        strg_mock = mock.Mock()
+        strg_mock.retrieve_instance.return_value = instance
+        client_mock = mock.Mock()
+        client_mock.destroyVirtualMachine.side_effect = Exception("wat", "wot")
+        manager = cloudstack.CloudStackManager(storage=strg_mock)
+        manager.client = client_mock
+        got_instance = manager.terminate_instance("some_instance")
+        self.assertEqual(instance, got_instance)
+        stderr.write.assert_called_with("[ERROR] Failed to terminate CloudStack VM: wat wot")
+
+    @mock.patch("uuid.uuid4")
+    def test_physical_scale_up(self, uuid):
+        self.set_api_envs()
+        self.addCleanup(self.del_api_envs)
+        self.set_vm_envs(project_id="project-123", network_id="net-123")
+        self.addCleanup(self.del_vm_envs)
+        uuid.return_value = "uuid_val"
+        instance = storage.Instance(name="some_instance",
+                                    units=[storage.Unit(id="123")])
+        strg_mock = mock.Mock()
+        client_mock = mock.Mock()
+        client_mock.deployVirtualMachine.return_value = {"id": "abc123",
+                                                         "jobid": "qwe321"}
+        client_mock.queryAsyncJobResult.return_value = {"jobstatus": 1}
+        vm = {"id": "qwe123", "nic": [{"ipaddress": "10.0.0.5"}]}
+        client_mock.listVirtualMachines.return_value = {"virtualmachine": [vm]}
+        client_mock.encode_user_data.return_value = user_data = mock.Mock()
+        manager = cloudstack.CloudStackManager(storage=strg_mock)
+        manager.client = client_mock
+        units = manager.physical_scale(instance, 2)
+        self.assertEqual(2, len(instance.units))
+        self.assertEqual(1, len(units))
+        unit = instance.units[1]
+        self.assertEqual("qwe123", unit.id)
+        self.assertEqual("uuid_val", unit.secret)
+        self.assertEqual(instance, unit.instance)
+        self.assertEqual("10.0.0.5", unit.dns_name)
+        self.assertEqual("creating", unit.state)
+        create_data = {"group": "feaas", "displayname": "feaas_some_instance",
+                       "templateid": self.template_id, "zoneid": self.zone_id,
+                       "serviceofferingid": self.service_offering_id,
+                       "userdata": user_data, "networkid": self.network_id,
+                       "projectid": self.project_id}
+        client_mock.deployVirtualMachine.assert_called_with(create_data)
+        actual_user_data = manager.get_user_data("uuid_val")
+        client_mock.encode_user_data.assert_called_with(actual_user_data)
+
+    def test_physical_scale_down(self):
+        self.set_api_envs()
+        self.addCleanup(self.del_api_envs)
+        units = [storage.Unit(id="vm-123"), storage.Unit(id="vm-456"),
+                 storage.Unit(id="vm-789")]
+        instance = storage.Instance(name="some_instance", units=copy.deepcopy(units))
+        strg_mock = mock.Mock()
+        manager = cloudstack.CloudStackManager(storage=strg_mock)
+        manager.client = client_mock = mock.Mock()
+        got_units = manager.physical_scale(instance, 1)
+        self.assertEqual(1, len(instance.units))
+        self.assertEqual(2, len(got_units))
+        self.assertEqual("vm-789", instance.units[0].id)
+        expected_calls = [mock.call({"id": "vm-123"}), mock.call({"id": "vm-456"})]
+        self.assertEqual(expected_calls, client_mock.destroyVirtualMachine.call_args_list)
+
+
+class MaxTryExceededErrorTestCase(unittest.TestCase):
+
+    def test_error_message(self):
+        exc = cloudstack.MaxTryExceededError(40)
+        self.assertEqual(40, exc.max_tries)
+        self.assertEqual(("exceeded 40 tries",), exc.args)
